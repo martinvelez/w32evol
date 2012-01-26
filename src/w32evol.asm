1f47207f85456c0943d448f1ea140604d44bf718
Martin Velez <mvelez999@gmail.com>
2012-01-25 18:12:54 -0800

    updated README

--- src/w32evol.asm
+++ src/w32evol.asm
@@ -1,4 +1,12 @@
-include \masm32\include\masm32rt.inc
+.386
+.model flat,stdcall
+option casemap:none
+
+include \masm32\include\windows.inc
+include \masm32\include\kernel32.inc
+include \masm32\include\user32.inc
+includelib \masm32\lib\kernel32.lib
+includelib \masm32\lib\user32.lib
 
 .data
 success							dd 0
